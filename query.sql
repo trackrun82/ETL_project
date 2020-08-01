ed9f5b3323ed93c3aad7bcfbe1fcc3e1ab0765a9
trackrun82 <64228800+trackrun82@users.noreply.github.com>
2020-08-01 10:37:53 -0600

    finalized notebook

--- query.sql
+++ query.sql
@@ -1,14 +1,18 @@
 -- Query to check successful load
 SELECT * FROM movie;
-
 SELECT * FROM person;
-
 SELECT * FROM job_title;
-
 SELECT * FROM movie_person_title_junction;
+SELECT * FROM country_origin;
+SELECT * FROM movie_country_junction;
+SELECT * FROM language;
+SELECT * FROM movie_language_junction;
+SELECT * FROM genre;
+SELECT * FROM movie_genre_junction;
+SELECT * FROM production_company;
 
--- Join tables on county_id
--- SELECT premise.id, premise.premise_name, county.county_name
--- FROM premise
--- INNER JOIN county
--- ON premise.county_id = county.id;
+-- Join tables on movie_id
+SELECT movie.movie_id, movie.movie_title, movie_person_title_junction.person_id
+FROM movie
+INNER JOIN movie_person_title_junction
+ON movie.movie_id = movie_person_title_junction.movie_id;
--- schema.sql
+++ schema.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS movie_person_title_junction;
+DROP TABLE IF EXISTS movie_genre_junction;
+DROP TABLE IF EXISTS movie_language_junction;
+DROP TABLE IF EXISTS movie_country_junction;
+DROP TABLE IF EXISTS production_company;
 DROP TABLE IF EXISTS movie;
 DROP TABLE IF EXISTS person;
 DROP TABLE IF EXISTS job_title;
+DROP TABLE IF EXISTS country_origin;
+DROP TABLE IF EXISTS language;
+DROP TABLE IF EXISTS genre;
+
 
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/D9jGD3
@@ -51,6 +59,62 @@ CREATE TABLE "movie_person_title_junction" (
      )
 );
 
+CREATE TABLE "genre" (
+    "genre_id" VARCHAR(100)   NOT NULL,
+    "genre_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_genre" PRIMARY KEY (
+        "genre_id"
+     )
+);
+
+CREATE TABLE "movie_genre_junction" (
+    "movie_id" VARCHAR(100)   NOT NULL,
+    "genre_id" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_movie_genre_junction" PRIMARY KEY (
+        "movie_id","genre_id"
+     )
+);
+
+CREATE TABLE "country_origin" (
+    "country_id" VARCHAR(100)   NOT NULL,
+    "country_name" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_country_origin" PRIMARY KEY (
+        "country_id"
+     )
+);
+
+CREATE TABLE "movie_country_junction" (
+    "movie_id" VARCHAR(100)   NOT NULL,
+    "country_id" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_movie_country_junction" PRIMARY KEY (
+        "movie_id","country_id"
+     )
+);
+
+CREATE TABLE "language" (
+    "language_id" VARCHAR(100)   NOT NULL,
+    "language" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_language" PRIMARY KEY (
+        "language_id"
+     )
+);
+
+CREATE TABLE "movie_language_junction" (
+    "movie_id" VARCHAR(100)   NOT NULL,
+    "language_id" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_movie_language_junction" PRIMARY KEY (
+        "movie_id","language_id"
+     )
+);
+
+CREATE TABLE "production_company" (
+	"company_id" VARCHAR(100)   NOT NULL,
+	"company_name" VARCHAR(255)   NOT NULL,
+	CONSTRAINT "pk_production_company" PRIMARY KEY (
+		"company_id"
+	)
+);
+
 ALTER TABLE "movie_person_title_junction" ADD CONSTRAINT "fk_movie_person_title_junction_movie_id" FOREIGN KEY("movie_id")
 REFERENCES "movie" ("movie_id");
 
@@ -59,4 +123,26 @@ REFERENCES "person" ("person_id");
 
 ALTER TABLE "movie_person_title_junction" ADD CONSTRAINT "fk_movie_person_title_junction_job_title_id" FOREIGN KEY("job_title_id")
 REFERENCES "job_title" ("job_title_id");
+
+ALTER TABLE "movie_genre_junction" ADD CONSTRAINT "fk_movie_genre_junction_movie_id" FOREIGN KEY("movie_id")
+REFERENCES "movie" ("movie_id");
+
+ALTER TABLE "movie_genre_junction" ADD CONSTRAINT "fk_movie_genre_junction_genre_id" FOREIGN KEY("genre_id")
+REFERENCES "genre" ("genre_id");
+
+ALTER TABLE "movie_country_junction" ADD CONSTRAINT "fk_movie_country_junction_movie_id" FOREIGN KEY("movie_id")
+REFERENCES "movie" ("movie_id");
+
+ALTER TABLE "movie_country_junction" ADD CONSTRAINT "fk_movie_country_junction_country_id" FOREIGN KEY("country_id")
+REFERENCES "country_origin" ("country_id");
+
+ALTER TABLE "movie_language_junction" ADD CONSTRAINT "fk_movie_language_junction_movie_id" FOREIGN KEY("movie_id")
+REFERENCES "movie" ("movie_id");
+
+ALTER TABLE "movie_language_junction" ADD CONSTRAINT "fk_movie_language_junction_language_id" FOREIGN KEY("language_id")
+REFERENCES "language" ("language_id");
+
+-- ALTER TABLE "production_company" ADD CONSTRAINT "fk_production_company_company_id" FOREIGN KEY("company_id")
+-- REFERENCES "movie" ("company_id");
+
 
